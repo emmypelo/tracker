e0f4c34b67a38391d99c65517dfe70a48192c22d
Code Leo <emmypeloguns@gmail.com>
2025-06-07 10:01:58 +0100

    patch error fixed

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const corsOptions = {
       ? ["https://tracker-rust-zeta.vercel.app"]
       : ["http://localhost:3000", "http://127.0.0.1:3000"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   exposedHeaders: ["Set-Cookie"],
 };
