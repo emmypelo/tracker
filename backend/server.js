92ec0b93036d3da88658ec9c345ef1eecfaf3359
Code Leo <emmypeloguns@gmail.com>
2025-06-12 17:33:57 +0100

    Final fix for today

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ app.use(express.json());
 const corsOptions = {
   // origin:"https://tracker.pingbyleo.space",
   origin:
-    process.env.MODE || process.env.NODE_ENV === "production"
-      ? "https://tracker.pingbyleo.space" || "https://www.pingbyleo.space"
-      : "http://localhost:5173",
+    process.env.MODE === "production" || process.env.NODE_ENV === "production"
+      ? ["https://tracker.pingbyleo.space", "https://www.pingbyleo.space"]
+      : ["http://localhost:5173"],
 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
